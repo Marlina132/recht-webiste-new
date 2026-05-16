b8c102c1b1d2f2caed7cf43f2fcbda1d139312d1
Marlina132 <127156693+Marlina132@users.noreply.github.com>
2026-05-16 11:39:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function play() {
+    // 1. Create a new audio object using the path to your file
+    const audio = new Audio('sounds/button sound.mp3'); 
+    
+    // 2. Play the sound
+    audio.play();
+}
